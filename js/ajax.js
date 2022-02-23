95c8a475d2ad90c90e886cdfa3e9ab8ab486d667
ltk <dlxorb1223@naver.com>
2022-02-23 19:49:43 +0900

    ajax 수정

--- js/ajax.js
+++ js/ajax.js
@@ -39,23 +39,26 @@ jQuery(document).ready(function(){
         type : "GET",            // HTTP method type(GET, POST) 형식이다.
         url : "js/webinfo.json",      // 컨트롤러에서 대기중인 URL 주소이다.
         data : "JSON",            // Json 형식의 데이터이다.
-        success : function(){ // 비동기통신의 성공일경우 success콜백으로 들어옵니다. 'res'는 응답받은 데이터이다.
+        success : function(data){ // 비동기통신의 성공일경우 success콜백으로 들어옵니다. 'res'는 응답받은 데이터이다.
             // 응답코드 > 0000
-            alert("성공");
+            
+            $.each(data, function(info) { // 데이터 =info
+                var info_img = info.img;
+                var info_title = info.title;
+                var info_text = info.text;
+                var img = $('<img />').attr("src", "img/"+info_img);
+                var title = $('<p />').text(info_title);
+                var text = $('<p />').text(info_text);
+                var infoCon = $('.info-con');
+                infoCon.append(img);
+                infoCon.append(title);
+                infoCon.append(text);
+                webinfo.append(infoCon);
+            }); 
         },
         error : function(){ // 비동기 통신이 실패할경우 error 콜백으로 들어옵니다.
             alert("통신 실패.")
         }    
     });
-    var info_img = info.img;
-    var info_title = info.title;
-    var info_text = info.text;
-    var img = $('<img />').attr("src", "img/"+info_img);
-    var title = $('<p />').text(info_title);
-    var text = $('<p />').text(info_text);
-    var infoCon = $('.info-con');
-    infoCon.append(img);
-    infoCon.append(title);
-    infoCon.append(text);
-webinfo.append(infoCon);
+ 
 });
