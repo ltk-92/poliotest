78c65b82ddfcaacbe5f11d236d2ca29d14bdaccd
ltk <dlxorb1223@naver.com>
2022-02-23 22:28:16 +0900

    15

--- js/ajax.js
+++ js/ajax.js
@@ -37,13 +37,13 @@ jQuery(document).ready(function(){
     
     var webinfo = $('.web-info');
     $.ajax({
-        type : "POST",            // HTTP method type(GET, POST) 형식이다.
+        type : "GET",            // HTTP method type(GET, POST) 형식이다.
         url : "https://ltk-92.github.io/poliotest//js/webinfo.json",      // 컨트롤러에서 대기중인 URL 주소이다.         
         dataType: "json", // Json 형식의 데이터이다.
-        success : function(data){ // 비동기통신의 성공일경우 success콜백으로 들어옵니다. 'res'는 응답받은 데이터이다.
+        success : function(info){ // 비동기통신의 성공일경우 success콜백으로 들어옵니다. 'res'는 응답받은 데이터이다.
             // 응답코드 > 0000
             
-            $.each(data, function(info) { // 데이터 =info
+         // 데이터 =info
                 var info_img = info.img;
                 var info_title = info.title;
                 var info_text = info.text;
@@ -55,7 +55,7 @@ jQuery(document).ready(function(){
                 infoCon.append(title);
                 infoCon.append(text);
                 webinfo.append(infoCon);
-            }); 
+           
         },
         error : function(){ // 비동기 통신이 실패할경우 error 콜백으로 들어옵니다.
             alert("통신 실패.")
